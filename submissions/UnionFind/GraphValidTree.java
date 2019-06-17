7971481918efeb45287d715bba287b841c674f96
Chen Rong <crong@google.com>
2019-06-17 11:02:48 -0700

    valid graph tree

---
+++ submissions/UnionFind/GraphValidTree.java
@@ -0,0 +1,60 @@
+/**
+ * https://www.lintcode.com/problem/graph-valid-tree/
+ * https://leetcode.com/problems/graph-valid-tree/
+ * 
+ * 给出 n 个节点，标号分别从 0 到 n - 1 并且给出一个 无向 边的列表 
+ * (给出每条边的两个顶点), 写一个函数去判断这张｀无向｀图是否是一棵树
+ */
+public class GraphValidTree {
+
+  class UnionFind {
+    int[] parent;
+    int size;
+
+    UnionFind(int n) {
+      this.parent = new int[n];
+      this.size = n;
+      for (int i = 0; i < n; i++) {
+        parent[i] = i;
+      }
+    }
+
+    int compressFind(int child) {
+      int copyChild = child;
+      while (copyChild != parent[copyChild]) {
+        copyChild = parent[copyChild];
+      }
+      int bigbro = copyChild;
+
+      int prevParent;
+      while (child != bigbro) {
+        prevParent = parent[child];
+        parent[child] = bigbro;
+        child = prevParent;
+      }
+      return bigbro;
+    }
+
+    void union(int a, int b) {
+      int aBigbro = compressFind(a);
+      int bBigbro = compressFind(b);
+      if (aBigbro != bBigbro) {
+        parent[aBigbro] = parent[bBigbro];
+        size--;
+      }
+    }
+  }
+
+  public boolean validTree(int n, int[][] edges) {
+    if (n <= 0 || edges.length != n - 1) {
+      return false;
+    }
+    UnionFind uf = new UnionFind(n);
+
+    for (int[] edge : edges) {
+      uf.union(edge[0], edge[1]);
+    }
+
+    return uf.size == 1;
+  }
+}
