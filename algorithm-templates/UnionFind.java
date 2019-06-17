24388d8a93d6151591166865255b087d279fc17f
Chen Rong <crong@google.com>
2019-06-16 17:01:56 -0700

    better naming for understanding

--- algorithm-templates/UnionFind.java
+++ algorithm-templates/UnionFind.java
@@ -1,31 +1,30 @@
+/**
+	底层数据结构
+	• 父亲表示法，用一个数组/哈希表记录每个节点的父亲是谁。
+	• father[“Nokia”] = “Microsoft”
+	• father[“Instagram”] = “Facebook”
+	查询所在集合
+	• 用所在集合最顶层的老大哥节点来代表这个集合
+	合并两个集合
+	• 找到两个集合中最顶层的两个老大哥节点 A 和 B
+	• father[A] = B // or father[B] = A 如果无所谓谁合并谁的话 
+*/
+
 public class UnionFind {
-	/**
-		底层数据结构
-		• 父亲表示法，用一个数组/哈希表记录每个节点的父亲是谁。
-		• father[“Nokia”] = “Microsoft”
-		• father[“Instagram”] = “Facebook”
-		查询所在集合
-		• 用所在集合最顶层的老大哥节点来代表这个集合
-		合并两个集合
-		• 找到两个集合中最顶层的两个老大哥节点 A 和 B
-		• father[A] = B // or father[B] = A 如果无所谓谁合并谁的话 
-	*/
+	public int size;
+	public int[] parent;
 
 	/**
 	 代码模板 - 初始化
-	 	使用哈希表或者数组来存储每个节点的父亲节点
-		如果节点不是连续整数的话，就最好用哈希表来存储
+ 	 	使用哈希表或者数组来存储每个节点的父亲节点
+		(如果节点不是连续整数的话，就最好用哈希表来存储)
 		最开始所有的父亲节点都指向自己
 	 */
-	private int size;
-	private int[] f;
-
-	public UnionFind(int n) {
-		this.size = n;
-		this.f = new int[size];
-
+	public UnionFind(int size) {
+		this.size = size;
+		this.parent = new int[size];
 		for (int i = 0; i < this.size; i++) {
-			f[i] = i;
+			this.parent[i] = i;
 		}
 	}
 
@@ -35,26 +34,27 @@ public class UnionFind {
 		代码模板 - 路径压缩
 			在找到老大哥以后，还需要把一路上经过的点都指向老大哥
 	 */
-	private int find(int x, int[] f) {
-		// 用新变量j代替x找老大哥
-		// 用新变量tmp来做swap
-		int j, tmp;
-		j = x;
+	private int find(int child, int[] parent) {
+		// 用新变量childCopy代替child找老大哥
+		int childCopy = child;
 		
-		// 找到老大哥
-		while (f[j] != j) {
-			j = f[j];
+		/** 找到老大哥 */
+		while (f[childCopy] != childCopy) {
+			childCopy = parent[childCopy];
 		}
+		// 用新变量bigbro作为返回值
+		int bigbro = childCopy;
 
-		// 把一路上经过的点都指向老大哥,
-		while(x != j) {
-			tmp = f[x]; // use tmp to save x's father
-			f[x] = j;   // point x's father to big bro
-			x = tmp;    // now change x to x's previous father
+		int prevParent;
+		/** 把一路上经过的点都指向老大哥, 路径压缩 */
+		while(child != bigbro) {
+			prevParent = parent[child]; // 用来prevParent记录之前的parent
+			parent[child] = bigbro;   // point child's father to 老大哥
+			child = prevParent;
 		}
 
 		// return老大哥
-		return j;
+		return bigbro;
 	}
 
 	/**
@@ -62,13 +62,13 @@ public class UnionFind {
 			找到两个元素所在集合的两个老大哥 A 和 B
 			将其中一个老大哥的父指针指向另外一个老大哥
 	 */
-	public void merge(int x, int y, int[] f) {
-		int fx = find(x, f);
-		int fy = find(y, f);
+	public void union(int a, int b, int[] parent) {
+		int a_bigbro = find(a, parent);
+		int b_bigbro = find(b, parent);
 
-		if (fx != fy) {
-			// 所有x的师兄弟 都和y的老大哥混了
-			f[fx] = fy;
+		if (a_bigbro != b_bigbro) {
+			// 所有a的师兄弟 都和b的老大哥混了
+			parent[a_bigbro] = b_bigbro;
 		}
 	}
 
