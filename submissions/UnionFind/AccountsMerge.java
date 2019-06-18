861f5a8ea717ee9e788a28e9e8c6c57d93eaf82d
Chen Rong <crong@google.com>
2019-06-18 13:01:48 -0700

    min stack

--- submissions/UnionFind/AccountsMerge.java
+++ submissions/UnionFind/AccountsMerge.java
@@ -1,13 +1,13 @@
 /**
  * https://www.lintcode.com/problem/accounts-merge/
  * https://leetcode.com/problems/accounts-merge/
- * 
+ *
  * 一种做法是将每个原始账户作为一个节点，
  * 但是两个账户可以连接取决于它们共享至少一个邮箱，处理起来比较麻烦
- * 
+ *
  * 我们可以将每个邮箱作为一个节点，同一个原始账户中的邮箱之间连边。
  * 并用老大哥节点存储用户名
- * 
+ *
  * 灵活定义并查集的节点
  */
 
