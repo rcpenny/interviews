1e0925fb2605890e3458452591ab67fd365494d6
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-06-18 20:46:13 -0700

    binary search prep

---
+++ submissions/BinarySearch/FindDuplicateNumber.java
@@ -0,0 +1,18 @@
+/**
+ * https://www.lintcode.com/problem/find-the-duplicate-number/
+ * 
+ * 给出一个数组 nums 包含 n + 1 个整数，每个整数是从 1 到 n (包括边界)，保证至少存在一个重复的整数。
+ * 假设只有一个重复的整数，找出这个重复的数。
+ * 
+ * 1.不能修改数组(假设数组只能读)
+ * 2.只能用额外的O(1)的空间
+ * 3.时间复杂度小于O(n^2)
+ * 4.数组中只有一个重复的数，但可能重复超过一次
+ */
+public class FindDuplicateNumber {
+  public int findDuplicate(int[] nums) {
+    return 1;
+  }
+
+  
+}
