2328cb07fc481fe53fa178560e7f1b0737dd4439
Chen Rong <crong@google.com>
2019-06-28 14:05:05 -0700

    remove dupes in array

---
+++ ladders/双指针/同向双指针/FastSlow/IntersectionTwoLinkedList.java
@@ -0,0 +1,53 @@
+/**
+ * Write a program to find the node at which the 
+ * intersection of two singly linked lists begins.
+ * 
+ * 
+ * If the two linked lists have no intersection at all, return null.
+ * The linked lists must retain their original structure after the function returns.
+ * You may assume there are no cycles anywhere in the entire linked structure.
+ */
+class ListNode {
+  int val;
+  ListNode next;
+  ListNode(int x) {
+    val = x;
+    next = null;
+  }
+}
+
+public class IntersectionTwoLinkedList {
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		if (headA == null || headB == null) return null;
+
+		ListNode tmpA = headA, tmpB = headB;
+		int lengthA = 0, lengthB = 0;
+
+		while (tmpA != null) {
+			tmpA = tmpA.next;
+			lengthA++;
+		}
+		while (tmpB != null) {
+			tmpB = tmpB.next;
+			lengthB++;
+		}
+
+		tmpA = headA;
+		tmpB = headB;
+		int diff = Math.abs(lengthA - lengthB);
+		if (lengthA >= lengthB) {
+			for (int i = 0; i < diff; i++) tmpA = tmpA.next;
+		} else {
+			for (int i = 0; i < diff; i++) tmpB = tmpB.next;
+		}
+
+		// move forward together
+		while (tmpA != null && tmpB != null) {
+			if (tmpA == tmpB) return tmpA;
+			tmpA = tmpA.next;
+			tmpB = tmpB.next;
+		}
+
+		return null;
+	}
+}
