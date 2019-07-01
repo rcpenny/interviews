0db7d950f478ad772454348a3c1df053ee249f4b
Chen Rong <crong@google.com>
2019-06-30 19:11:00 -0700

    move zeros

---
+++ ladders/双指针/同向双指针/FastSlow/MoveZeros.java
@@ -0,0 +1,24 @@
+/**
+ * Given an array nums, write a function to move all 0's to the end of it while 
+ * maintaining the relative order of the non-zero elements.
+ */
+public class MoveZeros {
+  public void moveZeroes(int[] nums) {
+		int fast = 0, slow = 0;
+
+		// move all non-zeros to the front
+		while (fast < nums.length) {
+			if (nums[fast] != 0) {
+				swap(nums, fast, slow);
+				slow++;
+			}
+			fast++;
+		}
+	}
+
+	private void swap(int[] nums, int a, int b) {
+		int tmp = nums[a];
+		nums[a] = nums[b];
+		nums[b] = tmp;
+	}
+}
