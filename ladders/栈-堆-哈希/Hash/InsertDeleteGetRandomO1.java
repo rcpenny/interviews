640281a5416edb52cec42f2b542e669f05a2e92c
Chen Rong <crong@google.com>
2019-07-03 16:57:29 -0700

    insert delete getrandom

---
+++ ladders/栈-堆-哈希/Hash/InsertDeleteGetRandomO1.java
@@ -0,0 +1,20 @@
+/**
+ * 设计一个数据结构实现在平均 O(1) 的复杂度下执行以下所有的操作。
+		insert(val): 如果这个元素不在set中，则插入。
+		remove(val): 如果这个元素在set中，则从set中移除。
+		getRandom: 随机从set中返回一个元素。每一个元素返回的可能性必须相同。
+ */
+
+public class InsertDeleteGetRandomO1 {
+	public InsertDeleteGetRandomO1() {
+	}
+
+	public boolean insert(int val) {
+	}
+
+	public boolean remove(int val) {
+	}
+
+	public int getRandom() {
+	}
+}
