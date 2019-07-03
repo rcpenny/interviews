f589c3487f2a79da523f55c6e8bd56c651c76979
Chen Rong <crong@google.com>
2019-07-03 14:43:15 -0700

    remove dupe find missing number 2

--- ladders/深度优先搜索/其他/FindMissingNumber2.java
+++ ladders/深度优先搜索/其他/FindMissingNumber2.java
@@ -1,46 +1,53 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * 给一个由 1 - n 的整数随机组成的一个字符串序列，其中丢失了一个整数，请找到它
- * 
- * 输入: n = 6 和 str = 56412
-		输出: 3
-		解释:
-		5'6'4'1'2
+ * Giving a string with number from 1-n in random order, but miss 1 number.Find that number.
+    Example
+    Example1
+
+    Input: n = 20 and str = 19201234567891011121314151618
+    Output: 17
+    Explanation:
+    19'20'1'2'3'4'5'6'7'8'9'10'11'12'13'14'15'16'18
  */
+
 public class FindMissingNumber2 {
-	private int missing = -1;
+	public int missing = -1;
 
   public int findMissing2(int n, String str) {
-		if (n < 1 || str == null) return -1;
-		find(str, 0, 0, n, new boolean[n + 1]);
+		if (n <= 0) return missing;
+
+		boolean[] found = new boolean[n + 1];
+		find(str, 0, 0, n, found);
+
 		return missing;
 	}
-
-	private void find(String str, int index, int num, int n, boolean[] states) {
-		if (index == str.length() && num == n - 1) {
-		  for (int i = 1; i <= n; i++) {
-		    if (!states[i]) missing = i;
-		  }
-			return;
+	
+	private void find(String str, int index, int count, int n, boolean[] found) {
+		// recursion exit 限制条件 count或index达到某个值
+		if ( count == n - 1 && index == str.length()) {
+			for (int i = 1; i < found.length; i++) if (!found[i]) missing = i;
 		}
-    
-    // n <= 30, so max 2 digits
-		for (int len = 1; len <= 2; len++) {
-		  if (index + len > str.length()) continue;
-		  
-			String sub = str.substring(index, index + len);
-			if (!isValidNumber(n, sub, states)) continue;
+		if (count >= n - 1 || index >= str.length()) return;
 
-      int val = Integer.valueOf(sub);
-			states[val] = true;
-			find(str, index + len, num + 1, n, states);
-			states[val] = false;
+		for (int len = 1; len <= 2; len++) {
+			// 进入下个递归前的额外操作：去重？去掉特殊条件？
+			if (index + len > str.length()) continue;
+			String sub = str.substring(index, index + len);
+			if (!isValidNumber(sub, n, found)) continue;
+			
+			int val = Integer.valueOf(sub);
+			found[val] = true;
+			find(str, index + len, count + 1, n, found);
+			found[val] = false;
 		}
 	}
 
-	private boolean isValidNumber(int n, String sub, boolean[] states) {
-	  // skip "06" case
-	  if (sub.charAt(0) == '0') return false;
-	  int val = Integer.valueOf(sub);
-	  return (val < 1 || val > n || states[val]) ? false :  true;
+	private boolean isValidNumber(String sub, int n, boolean[] found) {
+		// 去掉 “06”这样的case
+		if (sub.charAt(0) == '0') return false;
+		int val = Integer.valueOf(sub);
+		return (val > 0 && val <= n && !found[val]) ? true : false;
 	}
 }
