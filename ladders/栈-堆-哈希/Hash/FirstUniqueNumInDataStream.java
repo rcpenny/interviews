9c600fb06889b50f7df20a5a0fea627d403ab781
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-07-03 23:38:13 -0700

    moving avg

---
+++ ladders/栈-堆-哈希/Hash/FirstUniqueNumInDataStream.java
@@ -0,0 +1,12 @@
+// 给一个连续的数据流,写一个函数返回终止数字到达时的第一个唯一数字（包括终止数字）,
+// 如果找不到这个终止数字, 返回 -1.
+// 输入： 
+// [1, 2, 2, 1, 3, 4, 4, 5, 6]
+// 5
+// 输出： 3
+
+public class FirstUniqueNumInDataStream {
+  public int firstUniqueNumber(int[] nums, int number) {
+    
+  }
+}
