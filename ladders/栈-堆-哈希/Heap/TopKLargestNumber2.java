1b9f6e63e010d8234f94901360fa40cc7dd8d351
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-07-04 14:42:21 -0700

    sss

---
+++ ladders/栈-堆-哈希/Heap/TopKLargestNumber2.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+// Implement a data structure, provide two interfaces:
+
+// add(number). Add a new number in the data structure.
+// topk(). Return the top k largest numbers in this data structure. 
+// k is given when we create the data structure.
+
+public class TopKLargestNumber2 {
+  PriorityQueue<Integer> pq;
+
+  public TopKLargestNumber2(int k) {
+    this.pq = new PriorityQueue<>(k, Collections.reverseOrder());
+  }
+
+  public void add(int num) {
+    if (pq.size() < this.size) {
+      pq.offer(num);
+      return;
+    }
+    if (num > pq.peek()) {
+      pq.poll();
+      pq.offer(num);
+    }
+  }
+
+  public List<Integer> topk() {
+    List<Integer> topKNumbers = new ArrayList<>();
+    Iterator it = pq.iterator();
+    while (it.hasNext()) topKNumbers.add((Integer) it.next());
+    Collections.reverse(topKNumbers);
+    return topKNumbers;
+  }
+}
