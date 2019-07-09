a9c589fad80bda43c36cb9b9775cbefe25f7cd69
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-07-09 12:34:01 -0700

    update binary search on result

--- ladders/二分法-扫描线/4-二分答案/WoodCut.java
+++ ladders/二分法-扫描线/4-二分答案/WoodCut.java
@@ -5,23 +5,22 @@
  * 当然，我们希望得到的小段越长越好，你需要计算能够得到的小段木头的最大长度。
  */
 
+ // O(nlogL)
 public class WoodCut {
 
   public int woodCut(int[] L, int k) {
     if (L == null || L.length == 0) return 0;
 
     int maxLength = 0, minLength = 0;
-    // 找到木堆中最长的木头
+    // 找到木堆中最长的木头 可行解范围（0 -> 最长木头)
     for (int l : L) maxLength = Math.max(l, maxLength);
 
     while (minLength + 1 < maxLength) {
       int middleLength = minLength + (maxLength - minLength) / 2;
       int pieces = countPieces(L, middleLength);
-      if (pieces < k) {
-        maxLength = middleLength;
-      } else {
-        minLength = middleLength;
-      }
+
+      if (pieces < k) maxLength = middleLength;
+      else minLength = middleLength;
     }
 
     if (countPieces(L, maxLength) == k) return maxLength;
