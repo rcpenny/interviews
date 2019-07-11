86bae32dc91e35e244c7ee5775a8b032beba269f
Chen Rong <crong@google.com>
2019-07-11 11:47:19 -0700

    find dupe number no sorting no extra space

--- ladders/二分法-扫描线/4-二分答案/FindDuplicateNumber.java
+++ ladders/二分法-扫描线/4-二分答案/FindDuplicateNumber.java
@@ -8,11 +8,31 @@
  * 2.只能用额外的O(1)的空间
  * 3.时间复杂度小于O(n^2)
  * 4.数组中只有一个重复的数，但可能重复超过一次
+ * 输入: [5,4,4,3,2,1]
+ * 输出:  4
  */
-public class FindDuplicateNumber {
-  public int findDuplicate(int[] nums) {
-    return 1;
-  }
 
-  
+ // 思路：大于4的数字应该是2个，但是只有1个.所以4是有问题的.
+public class FindDuplicateNumber {
+	public int findDuplicate(int[] nums) {
+		int l = 1;
+		int r = nums.length - 1;  // n
+		
+		// O(logN)
+		while (l + 1 < r) {
+			int mid = l + (r - l) / 2;
+			if (count(nums, mid) <= mid) l = mid;
+			else r = mid;
+		}
+		
+		if (count(nums, l) <= l) return r;
+		return l;
+	}
+
+	// O(n)
+	private int count(int[] nums, int mid) {
+		int cnt = 0;
+		for (int item : nums) if (item <= mid) nt++;
+		return cnt;
+	}
 }
