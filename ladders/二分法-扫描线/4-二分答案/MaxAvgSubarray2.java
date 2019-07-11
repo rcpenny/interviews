aa390d8e702fa342b83db953683f652999c386a1
Chen Rong <crong@google.com>
2019-07-11 13:32:31 -0700

    max subarray

---
+++ ladders/二分法-扫描线/4-二分答案/MaxAvgSubarray2.java
@@ -0,0 +1,11 @@
+// Given an array with positive and negative numbers, 
+// find the maximum average subarray which length should be greater or equal to given length k.
+
+// Input:  [1,12,-5,-6,50,3] 3
+// Output: 15.667
+
+public class MaxAvgSubarray2 {
+	public double maxAverage(int[] nums, int k) {
+		
+	}
+}
---
+++ ladders/字符串-数组/数组/MaxSubarray.java
@@ -0,0 +1,22 @@
+// 给定一个整数数组，找到一个具有最大和的子数组，返回其最大和
+// 输入：[−2,2,−3,4,−1,2,1,−5,3]
+// 输出：6
+// 解释：符合要求的子数组为[4,−1,2,1]，其最大和为 6
+
+public class MaxSubarray {
+  public int maxSubArray(int[] nums) {
+		int prefix_sum = 0;
+		int min_sum_sofar = 0; // 
+		int max_sum = Integer.MIN_VALUE;
+
+		for (int i = 0; i < nums.length; i++) {
+			prefix_sum = prefix_sum + nums[i];
+
+			// 先算此点前缀和，与前一个点的min_sum.
+			max_sum = Math.max(max_sum, prefix_sum - min_sum_sofar);
+			min_sum_sofar = Math.min(min_sum_sofar, prefix_sum);
+		}
+
+		return max_sum;
+	}
+}
