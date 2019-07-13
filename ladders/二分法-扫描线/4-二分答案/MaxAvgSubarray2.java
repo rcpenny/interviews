51f5b36257d79c406637ceaf71b8b85cd82ec1c4
Chen Rong <crong@google.com>
2019-07-13 13:59:05 -0700

    max avg subarray hint

--- ladders/二分法-扫描线/4-二分答案/MaxAvgSubarray2.java
+++ ladders/二分法-扫描线/4-二分答案/MaxAvgSubarray2.java
@@ -4,7 +4,11 @@
 // Input:  [1,12,-5,-6,50,3] 3
 // Output: 15.667
 
-@Todo
+// 如果最大平均值是T
+// 那么 (A[left] + ... + A[right]) / (right - left + 1) >= T
+// 转换成 (A[left] - T) + ... + (A[right] - T) >= 0     right - left + 1 >= k
+ 
+@Todo("此题前缀和暴力可解，更好的是用二分答案")
 public class MaxAvgSubarray2 {
 	public double maxAverage(int[] nums, int k) {
 		
