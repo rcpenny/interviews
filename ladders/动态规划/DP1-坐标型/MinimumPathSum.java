880b55298b43a1c7e1cfa0c5c263b3cb6c78ba94
Chen Rong <crong@google.com>
2019-07-14 16:23:07 -0700

    滚动数组

--- ladders/动态规划/DP1-坐标型/MinimumPathSum.java
+++ ladders/动态规划/DP1-坐标型/MinimumPathSum.java
@@ -5,27 +5,32 @@
 // 初态边界: f[0][0] = grid[0][0] i = 0 || j = 0
 // 计算顺序:
 
+// follow up : 空间优化 滚动数组
 public class MinimumPathSum {
 	public int minPathSum(int[][] grid) {
 		if (grid == null ||  grid.length == 0 || grid[0].length == 0)
 			return 0;
 
-		int row = grid.length;
-		int col = grid[0].length;
-
-		int[][] f = new int [row][col];
-		f[0][0] = grid[0][0];
+		int row = grid.length, col = grid[0].length;
+		int old = 0, now = 0;
+		int[][] f = new int [2][col];
 
 		for (int i = 0; i < row; i++) {
+		  old = now;
+		  now = 1 - now;
+		  
 			for (int j = 0; j < col; j++) {
-				if (i == 0 && j == 0) continue;
-				f[i][j] = Integer.MAX_VALUE;
+				if (i == 0 && j == 0) {
+				  f[now][j] = grid[0][0];
+				  continue;
+				}
+				f[now][j] = Integer.MAX_VALUE;
 
-				if (i > 0) f[i][j] = Math.min(f[i][j], f[i - 1][j] + grid[i][j]);
-				if (j > 0) f[i][j] = Math.min(f[i][j], f[i][j - 1] + grid[i][j]);
+				if (i > 0) f[now][j] = Math.min(f[now][j], f[old][j] + grid[i][j]);
+				if (j > 0) f[now][j] = Math.min(f[now][j], f[now][j - 1] + grid[i][j]);
 			}
 		}
 
-		return f[row - 1][col - 1];
+		return f[now][col - 1];
 	}
 }
