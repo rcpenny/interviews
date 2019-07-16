a71d4518fd241dc869de73c3c866f18c1b8fdafa
Chen Rong <crong@google.com>
2019-07-16 14:50:12 -0700

    trie service

---
+++ company/Google/HARD/DesignSearchAutocompleteSystem.java
@@ -0,0 +1,20 @@
+// LC642
+// https://leetcode.com/problems/design-search-autocomplete-system/
+
+@Todo("July 16")
+class AutocompleteSystem {
+
+	public AutocompleteSystem(String[] sentences, int[] times) {
+			
+	}
+	
+	public List<String> input(char c) {
+			
+	}
+}
+
+/**
+* Your AutocompleteSystem object will be instantiated and called as such:
+* AutocompleteSystem obj = new AutocompleteSystem(sentences, times);
+* List<String> param_1 = obj.input(c);
+*/
