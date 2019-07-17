dd34fe46ff1b5c7b6893e7077cd6b77b245ee3de
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-07-16 21:05:23 -0700

    typeahead

---
+++ company/Facebook/HARD/Typeahead.java
@@ -0,0 +1,22 @@
+// 实现一个typeahead功能，给出一个字符串和一个字典，包含若干个单词，
+// 返回所有含有这个字符串子串的单词；字典不能被修改，并且这个方法可能被调用多次
+
+// 输入:
+// dict=["Jason Zhang","James Yu","Li Zhang","Yanxin Shi"]
+// search("Zhang")
+// search("James")
+
+// 输出:
+// ["Jason Zhang","Li Zhang"]
+// ["James Yu"]
+
+@Todo("下次怜惜Trie的时候")
+public class Typeahead {
+  public Typeahead(Set<String> dict) {
+    
+  }
+
+  public List<String> search(String str) {
+    
+  }
+}
