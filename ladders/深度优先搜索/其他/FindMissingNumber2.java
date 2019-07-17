e836680c6c792c7663c4ee7ee354536765a78221
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-07-16 22:46:52 -0700

    comment..

--- ladders/深度优先搜索/其他/FindMissingNumber2.java
+++ ladders/深度优先搜索/其他/FindMissingNumber2.java
@@ -1,16 +1,10 @@
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * Giving a string with number from 1-n in random order, but miss 1 number.Find that number.
-    Example
-    Example1
-
-    Input: n = 20 and str = 19201234567891011121314151618
-    Output: 17
-    Explanation:
-    19'20'1'2'3'4'5'6'7'8'9'10'11'12'13'14'15'16'18
- */
+//  Giving a string with number from 1-n in random order, 
+//  but miss 1 number.Find that number.
+// Input: n = 20 and str = 19201234567891011121314151618
+// Output: 17
 
 public class FindMissingNumber2 {
 	public int missing = -1;
