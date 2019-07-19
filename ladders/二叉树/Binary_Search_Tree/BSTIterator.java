536ad1fb67c65f14d3edcc6a7a92c8d33a67ea5a
Chen Rong <crong@google.com>
2019-07-19 13:10:48 -0700

    bst iterator

--- ladders/二叉树/Binary_Search_Tree/BSTIterator.java
+++ ladders/二叉树/Binary_Search_Tree/BSTIterator.java
@@ -10,16 +10,16 @@ import java.util.Stack;
 	*    do something for node
 	* }
 	
-stack 中保存一路走到当前节点的所有节点，stack.peek() 一直指向 iterator 指向的当前节点。
-因此判断有没有下一个，只需要判断 stack 是否为空
-获得下一个值，只需要返回 stack.peek() 的值，并将 stack 进行相应的变化，挪到下一个点。
+	stack 中保存一路走到当前节点的所有节点，stack.peek() 一直指向 iterator 指向的当前节点。
+	因此判断有没有下一个，只需要判断 stack 是否为空
+	获得下一个值，只需要返回 stack.peek() 的值，并将 stack 进行相应的变化，挪到下一个点。
 
-挪到下一个点的算法如下：
+	挪到下一个点的算法如下：
 
-如果当前点存在右子树，那么就是右子树中“一路向左下”的那个点
-如果当前点不存在右子树，则是走到当前点的路径中，第一个左拐的点
-访问所有节点用时O(n)，所以均摊下来访问每个节点的时间复杂度时O(1)
-	*/
+	如果当前点存在右子树，那么就是右子树中“一路向左下”的那个点
+	如果当前点不存在右子树，则是走到当前点的路径中，第一个左拐的点
+	访问所有节点用时O(n)，所以均摊下来访问每个节点的时间复杂度时O(1)
+*/
 
 class TreeNode {
 	public int val;
@@ -30,6 +30,7 @@ class TreeNode {
 	}
 }
 
+// 多写几遍
 public class BSTIterator {
 	private Stack<TreeNode> stack = new Stack<>();
 
@@ -46,31 +47,21 @@ public class BSTIterator {
 
 	// return stack peek(current min in BST), then move stack peek to next node(min)
 	public TreeNode next() {
-		TreeNode currentNode = stack.peek();
-		TreeNode node = currentNode;
+		TreeNode result = stack.peek();
+		TreeNode node = stack.peek();
 
-		if (node.right == null) { 
-			// -1的情况和6的情况，-1在左🌲，1即是下一个，6在右🌲，1已经被访问过，返回到10
-			// 则是走到当前点的路径中，第一个左拐的点
-			node = stack.pop();
-			while (!stack.empty() && stack.peek().right == node) {
-				node = stack.pop();
-			}
-		} else if (node.right != null) {
-			// 1的情况
+		if (node.right != null) {
 			node = node.right;
 			while (node != null) {
 				stack.push(node);
 				node = node.left;
 			}
+		} else if (node.right == null) {
+			node = stack.pop();
+			while (!stack.isEmpty() && stack.peek().right == node)
+				node = stack.pop();
 		}
-		return currentNode;
+
+		return result;
 	}
 }
-/**
-      10
-  	  /\
- 	   1 11
-    /	\  \
-  -1 	 6  12
- */
