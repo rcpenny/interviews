98d84bf0660df11ceefe0ac37e68133efb3597ba
Chen Rong <crong@google.com>
2019-07-19 11:27:07 -0700

    imrpove strcuture

--- ladders/二叉树/Binary_Search_Tree/KthSmallestElemBST.java
+++ ladders/二叉树/Binary_Search_Tree/KthSmallestElemBST.java
@@ -22,20 +22,6 @@ public class KthSmallestElemBST {
 
 			// 首先判断这个node有没有右子树
 
-			// 这个node的右子树为空，说明对于这个node作为root，没有更大的可以加了，向上回溯.
-			// 如果是在node 3 在这个/枝上         如果node 3在 \ 这个枝上    10
-			//				5                                1              /
-			//			/																		 \           1
-			//    3                                        3           \
-			// 直接 pop() 下一个peek就是5了       一路pop()到拐点            3
-			if (node.right == null) {
-				node = stack.pop();
-				while (!stack.isEmpty() && stack.peek().right == node) {
-					node = stack.pop();
-				}
-				continue;
-			}
-
 			// 右子树不为空，说明还有更大的可以push.
 			if (node.right != null) {
 				node = node.right;
@@ -43,6 +29,19 @@ public class KthSmallestElemBST {
 					stack.push(node);
 					node = node.right;
 				}
+				continue;
+			}
+
+			// 这个node的右子树为空，说明对于这个node作为root，没有更大的可以加了，向上回溯.
+			// 如果是在node 3 在这个/枝上         如果node 3在 \ 这个枝上    10
+			//				5                                1              /
+			//			/																		 \           1
+			//    3                                        3          \
+			// 直接 pop() 下一个peek就是5了       一路pop()到拐点10          3
+			if (node.right == null) {
+				node = stack.pop();
+				while (!stack.isEmpty() && stack.peek().right == node)
+					node = stack.pop();
 			}
 		}
 
