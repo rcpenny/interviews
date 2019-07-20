5a491fcbac6d837271d90849c0f0f5aeee057a13
Chen Rong <crong@google.com>
2019-07-20 16:59:47 -0700

    .... shoot. fast slow

---
+++ ladders/双指针/-> ->双指针/FastSlow/LongestSubstringNoRepeatChars.java
@@ -0,0 +1,30 @@
+// 给定一个字符串，请找出其中无重复字符的最长子字符串
+
+public class LongestSubstringNoRepeatChars {
+
+	public int lengthOfLongestSubstring(String s) {
+		int longest = 0;
+		if (s == null || s.length() == 0) return longest;
+
+		int fast = 0;
+		int slow = 0;
+		int[] letters = new int[256];
+		char[] array = s.toCharArray();
+
+		for (slow = 0; slow < array.length; slow++) {
+			
+			while (fast < array.length) {
+				char head = array[fast];
+        if (letters[head] != 0) break;
+        letters[head] = 1;
+        longest = Math.max(longest, fast - slow + 1);
+        fast++;
+			}
+
+			char tail = array[slow];
+			letters[tail] = 0;
+		}
+
+		return longest;
+	}
+}
