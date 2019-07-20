cd7537292d3ee8ec8f9e95c808aebc638982e138
Chen Rong <crong@google.com>
2019-07-20 13:19:55 -0700

    merge k sorted list

--- ladders/栈-堆-哈希/Heap/MergeKSortedList.java
+++ ladders/栈-堆-哈希/Heap/MergeKSortedList.java
@@ -3,49 +3,30 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 // 合并k个排序链表，并且返回合并后的排序链表。尝试分析和描述其复杂度。
-
-// Example
-// 样例 1:
 // 	输入:   [2->4->null,null,-1->null]
 // 	输出:  -1->2->4->null
 
-// 样例 2:
-// 	输入: [2->6->null,5->null,7->null]
-// 	输出:  2->5->6->7->null
-
-class ListNode {
-  int val;
-  ListNode next;
-  ListNode(int x) {
-    val = x;
-    next = null;
-  }
-}
-
 public class MergeKSortedList {
-	private Comparator<ListNode> cpt = new Comparator<ListNode>() {
-		@Override public int compare(ListNode a, ListNode b) {return a.val - b.val;}
-	};
 
-  public ListNode mergeKLists(List<ListNode> lists) {  		
-		PriorityQueue<ListNode> heap = new PriorityQueue<ListNode>(lists.size(), cpt);
+  private Comparator<ListNode> cpt = new Comparator<ListNode>() {
+    @Override public int compare(ListNode a, ListNode b) {return a.val - b.val;}
+  };
 
-		// add head listnode to heap
-		for (ListNode cur : lists) if (cur != null) heap.offer(cur);
-
-		// 生成List的方法, dummy, 
-		ListNode dummy = new ListNode(0);
-		ListNode tail = dummy;
-
-		while (!heap.isEmpty()) {
-			ListNode tmp = heap.poll();
+  public ListNode mergeKLists(List<ListNode> lists) {
+    ListNode dummy = new ListNode(0);
+    ListNode tail = dummy;
+    
+    PriorityQueue<ListNode> minheap = new PriorityQueue<ListNode>(cpt);
+    for (ListNode head : lists)
+      if (head != null) minheap.offer(head);
+    
+    while (!minheap.isEmpty()) {
+      ListNode tmp = minheap.poll();
 			tail.next = tmp;
-			tail = tmp;
-	
-			if (tmp.next == null) continue;
-			heap.offer(tmp.next);
-		}
-
-		return dummy.next;
-	}
+			tail = tail.next;
+      if (tmp.next != null) minheap.offer(tmp.next);      
+    }
+    
+    return dummy.next;
+  }
 }
