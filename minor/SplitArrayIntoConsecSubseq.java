211a8894f24bef68a37373bbc6d013653db87faf
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-07-24 21:06:43 -0700

    edit distance

--- minor/SplitArrayIntoConsecSubseq.java
+++
@@ -1,9 +0,0 @@
-/**
- * https://www.lintcode.com/problem/split-array-into-subsequences-containing-continuous-elements/description
- */
-final class SplitArrayIntoConsecSubseq {
-
-  private SplitArrayIntoConsecSubseq() {
-  }
-
-}
