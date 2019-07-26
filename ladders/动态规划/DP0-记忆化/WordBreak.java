fb988aa0c9379badb7265ba3f9ce3804178cd443
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-07-25 20:54:55 -0700

    WildcardMatching

---
+++ ladders/动态规划/DP0-记忆化/WordBreak.java
@@ -0,0 +1,8 @@
+// 给定字符串 s 和单词字典 dict，确定 s 是否可以分成一个或多个以空格分隔的子串，并且这些子串都在字典中存在。
+// 样例 1: 输入:  "lintcode", ["lint", "code"] 输出:  true
+
+public class WordBreak {
+  public boolean wordBreak(String s, Set<String> dict) {
+    
+  }
+}
