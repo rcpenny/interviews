8b00c20f9b1f47caf991341aeee6e5de2b8db563
Chen Rong <crong@google.com>
2019-07-29 10:19:14 -0700

    move zeros 总结

---
+++ ladders/动态规划/DP6-双序列型/DistinctSubseq.java
@@ -0,0 +1,9 @@
+/**
+ *
+ */
+final class DistinctSubseq {
+
+  private DistinctSubseq() {
+  }
+
+}
--- ladders/双指针/-> ->双指针/FastSlow/MoveZeros.java
+++ ladders/双指针/-> ->双指针/FastSlow/MoveZeros.java
@@ -2,11 +2,12 @@
  * Given an array nums,  move all 0's to the end of it while 
  * maintaining the relative order of the non-zero elements.
  */
+
+ // 反过来想，把所有non-zero拿到front去，快慢双指针
 public class MoveZeros {
   public void moveZeroes(int[] nums) {
 		int fast = 0, slow = 0;
 
-		// move all non-zeros to the front
 		while (fast < nums.length) {
 			if (nums[fast] != 0) {
 				swap(nums, fast, slow);
