4db9ac5af6b6955e021d511985bb07088f8f11bb
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-07-30 20:35:52 -0700

    二分ooxx复习，主要就是一个last first position

--- ladders/二分法-扫描线/2-叉叉圈圈/FindBadVersion.java
+++ ladders/二分法-扫描线/2-叉叉圈圈/FindBadVersion.java
@@ -1,12 +1,16 @@
 // find 1st position of target...
+
 public class FindBadVersion {
+  
   public int findFirstBadVersion(int n) {
     int start = 1, end = n;
     
     while (start + 1 < end) {
       int middle = start + (end - start) / 2;
-      if (SVNRepo.isBadVersion(middle)) end = middle;
-      else tart =middle;
+      if (SVNRepo.isBadVersion(middle)) 
+        end = middle;
+      else 
+        start =middle;
     }
     
     if (SVNRepo.isBadVersion(start)) return start;
--- ladders/二分法-扫描线/2-叉叉圈圈/FirstPositionOfTarget.java
+++ ladders/二分法-扫描线/2-叉叉圈圈/FirstPositionOfTarget.java
@@ -2,17 +2,20 @@
 // Return -1 if target does not exist.
 
 public class FirstPositionOfTarget {
+
   public int firstPosition(int[] nums, int target) {
     if (nums == null || nums.length == 0) return -1;
 
     int start = 0, end = nums.length - 1;
     while (start + 1 < end) {
       int middle = start + (end - start) / 2;
-      if (nums[middle] >= target) end = middle;
-      else start = middle;
+      if (nums[middle] >= target) 
+        end = middle;
+      else 
+        start = middle;
     }
 
-    // 连续重复数字，先查first
+    // FIRST POSITION，先查first
     if (nums[start] == target) return start;
     if (nums[end] == target) return end;
     return -1;
