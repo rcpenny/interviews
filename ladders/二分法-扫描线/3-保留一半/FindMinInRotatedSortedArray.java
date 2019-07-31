f3ac5b29fd9ac46c4a536bff0d63859381e820cb
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-07-30 20:57:01 -0700

    保留一半 复习

--- ladders/二分法-扫描线/3-保留一半/FindMinInRotatedSortedArray.java
+++ ladders/二分法-扫描线/3-保留一半/FindMinInRotatedSortedArray.java
@@ -1,6 +1,6 @@
 // Suppose a sorted array is rotated at some pivot unknown to you beforehand.
 // (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
-// You can assume no duplicate exists in the array.
+// no duplicate exists in the array.
 
 //      5               O 
 //    4               O
@@ -11,14 +11,17 @@
 // (WRONG: First position <= or < First Number)
 
 public class FindMinInRotatedSortedArray {
+
   public int findMin(int[] nums) {
     int tail = nums[nums.length - 1];
     int start = 0, end = nums.length - 1;
     
     while (start + 1 < end) {
       int middle = start + (end - start) / 2;
-      if (nums[middle] <= tail) end = middle;
-      else start = middle;
+      if (nums[middle] > tail) 
+        start = middle;
+      else 
+        end = middle;
     }
 
     return Math.min(nums[start], nums[end]);
--- ladders/二分法-扫描线/3-保留一半/FindMinInRotatedSortedArray2.java
+++ ladders/二分法-扫描线/3-保留一半/FindMinInRotatedSortedArray2.java
@@ -1,16 +1,18 @@
 //同上题，但是contain dupes
 
 public class FindMinInRotatedSortedArray2 {
+
   public int findMin(int[] nums) {
     int tail = nums[nums.length - 1];
     int head = nums[0];
 
-    // 多考虑一个首位同数的情况，导致第一次二分你无法判断在哪个区间.
+    // 多考虑一个首位同数的情况，导致第一次二分你无法判断在哪个区间. 只能循环一遍
     // 如 [1, 1, 1, 1, 1, 1, 1, 1, 1, 20, -5, 2, 1, 1]
     if (tail == head) {
-      for (int i = 0; i < nums.length - 1; i++) {
-        if (nums[i + 1] < nums[i]) return nums[i + 1];
-      }
+      for (int i = 0; i < nums.length - 1; i++)
+        if (nums[i] > nums[i + 1]) 
+          return nums[i + 1];
+
       return tail;
     }
 
@@ -18,8 +20,10 @@ public class FindMinInRotatedSortedArray2 {
     
     while (start + 1 < end) {
       int middle = start + (end - start) / 2;
-      if (nums[middle] <= tail) end = middle;
-      else start = middle;
+      if (nums[middle] > tail) 
+        start = middle;
+      else 
+        end = middle;
     }
 
     return Math.min(nums[start], nums[end]);
