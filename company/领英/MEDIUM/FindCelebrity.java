d87af8ec1c26e3344c903035fb3d05084676dbe5
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-08-17 14:47:35 -0700

    celebrity

--- company/领英/MEDIUM/FindCelebrity.java
+++ company/领英/MEDIUM/FindCelebrity.java
@@ -1,11 +1,7 @@
-import java.util.HashSet;
-import java.util.Set;
-
 // 假设你和 n 个人在一个聚会中(标记为 0 到 n - 1)，其中可能存在一个名人。
 // 名人的定义是所有其他 n - 1 人都认识他/她，但他/她不知道任何一个。
-// 现在你想要找出这个名人是谁或者验证这个名人不存在。
+// 现在你想要找出这个名人是谁或者验证这个名人不存在
 // 你唯一可以做的事情就是提出如下问题：“你好，A，你认识B吗？” 来获取A是否认识B。
-// 您需要通过询问尽可能少的问题(以渐近的意义)来找出名人是谁(或验证其不存在)。
 // 你得到一个辅助函数 bool know(a，b)，它会告诉你A是否知道B.
 // 实现一个函数 int findCelebrity(n)，你的函数应该使 knows 的调用次数最少。
 
@@ -14,6 +10,18 @@ import java.util.Set;
 
 public class FindCelebrity {
   public int findCelebrity(int n) {
-		
-	}
+    if (n <= 0) return -1;
+    if (n == 1) return 0;
+
+    int candi = 0;
+    for (int i = 1; i < n; i++)
+      if (knows(candi, i)) candi = i; 
+      // key point: 如果 candi认识其他人，直接pass, i被其他人认识，也被pass
+
+    for (int i = 0; i < n; ++i) {
+      if (i == candi) continue;
+      if (knows(candi, i) || !knows(i, candi)) return -1;
+    }
+    return candi;
+  }
 }
