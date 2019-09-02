7f39cd7476d77b261af85ec124349aa4b8d0eb8b
Chen Rong <crong@google.com>
2019-09-02 15:02:10 -0700

    max stack

---
+++ company/领英/EASY/MaxStack.java
@@ -0,0 +1,63 @@
+import java.util.Stack;
+
+// Design a max stack that supports push, pop, top, peekMax and popMax.
+
+// push(x) -- Push element x onto stack.
+// pop() -- Remove the element on top of the stack and return it.
+// top() -- Get the element on the top.
+// peekMax() -- Retrieve the maximum element in the stack.
+// popMax() -- Retrieve the maximum element in the stack, and remove it. If you find more than one maximum elements, only remove the top-most one.
+
+
+// Note:
+// -1e7 <= x <= 1e7
+// Number of operations won't exceed 10000.
+// The last four operations won't be called when stack is empty.
+
+class MaxStack {
+	private Stack<Integer> realStack;
+	private Stack<Integer> maxStack;
+	
+	public MaxStack() {
+		this.realStack = new Stack<>();
+		this.maxStack = new Stack<>();
+	}
+	
+	public void push(int x) {
+		realStack.push(x);
+		if (maxStack.isEmpty()) maxStack.push(x);
+		else maxStack.push(Math.max(maxStack.peek(), x));
+	}
+
+	public int pop() {
+		maxStack.pop();
+		return realStack.pop();
+	}
+	
+	public int top() {
+		return realStack.peek();
+	}
+	
+	public int peekMax() {
+		return maxStack.peek();
+	}
+	
+	public int popMax() {
+		Stack<Integer> tmp = new Stack<>();
+		int cur_max = peekMax();
+
+		while (top() != cur_max) {
+			tmp.push(realStack.pop());
+			maxStack.pop();
+		}
+
+		realStack.pop();
+		maxStack.pop();
+
+		while (!tmp.isEmpty()) {
+			push(tmp.pop());
+		}
+
+		return cur_max;
+	}
+}
