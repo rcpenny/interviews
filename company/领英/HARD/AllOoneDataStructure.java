a6db2f65647a057e396adad30256f0f253e660b2
Chen Rong <crong@google.com>
2019-09-14 18:04:45 -0700

    hmmm 就是这么骚

--- company/领英/HARD/AllOoneDataStructure.java
+++ company/领英/HARD/AllOoneDataStructure.java
@@ -9,30 +9,109 @@ import java.util.Map;
 // 所有的操作都需要 O(1) 的时间复杂度。
 // lint1245
 
-class AllOoneDataStructure {
-  Map<String, Integer> keyToFreq;
+public class AllOoneDataStructure {}
 
-  public AllOne() {
-    this.keyToFreq = new HashMap<>();
-  }
+// 用来存储这个freq对应哪些key
+class Bucket {
+	int count;
+	Set<String> keySet;
+	Bucket prev;
+	Bucket next;
+	public Bucket(int cnt) {
+		count = cnt;
+		keySet = new HashSet<>();
+	}
+}
 
-  /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
-  public void inc(String key) {
-    
-  }
-  
-  /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
-  public void dec(String key) {
-      
-  }
-  
-  /** Returns one of the keys with maximal value. */
-  public String getMaxKey() {
-      
-  }
-  
-  /** Returns one of the keys with Minimal value. */
-  public String getMinKey() {
-      
-  }
+public class AllOne {
+	private Bucket head;  // head和tail什么都不存,只是负责指向, buffer
+	private Bucket tail;
+	private Map<String, Integer> keyToCount;     // key -> count
+	private Map<Integer, Bucket> countToBucket;  // count -> bucket
+
+	public AllOne() {
+			head = new Bucket(Integer.MIN_VALUE);
+			tail = new Bucket(Integer.MAX_VALUE);
+			head.next = tail;
+			tail.prev = head;
+			countToBucket = new HashMap<>();
+			keyToCount = new HashMap<>();
+	}
+	
+	/** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
+	public void inc(String key) {
+		if (keyToCount.containsKey(key)) {   //如果存在，执行+1操作即可
+				changeKey(key, 1);
+		} else {
+			keyToCount.put(key, 1);      //如果不存在，创建即可
+			if (head.next.count != 1) {
+				addBucketAfter(new Bucket(1), head);
+			}
+			head.next.keySet.add(key);
+			countToBucket.put(1, head.next);
+		}
+	}
+	
+	/** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
+	public void dec(String key) {
+		if (keyToCount.containsKey(key)) {
+			int count = keyToCount.get(key);
+			if (count == 1) {        //如果刚好为1，移除即可
+				keyToCount.remove(key);
+				removeKeyFromBucket(countToBucket.get(count), key);
+			} else {
+				changeKey(key, -1);		//执行-1操作
+			}
+		}
+	}
+	
+	/** Returns one of the keys with maximal value. */
+	public String getMaxKey() {
+		if (tail.prev == head) return "";
+		return (String) tail.prev.keySet.iterator().next();
+	}
+
+	/** Returns one of the keys with Minimal value. */
+	public String getMinKey() {
+		if (head.next == tail) return "";
+		return (String) head.next.keySet.iterator().next();        
+	}
+
+	private void changeKey(String key, int offset) {  
+		int count = keyToCount.get(key);
+		keyToCount.put(key, count + offset);
+		Bucket curBucket = countToBucket.get(count);
+		Bucket newBucket;
+		if (countToBucket.containsKey(count + offset)) {
+			newBucket = countToBucket.get(count + offset);
+		} else {
+			newBucket = new Bucket(count + offset);
+			countToBucket.put(count + offset, newBucket);
+			addBucketAfter(newBucket, offset == 1 ? curBucket : curBucket.prev);
+		}
+		newBucket.keySet.add(key);
+		removeKeyFromBucket(curBucket, key);
+	}
+	
+	private void removeKeyFromBucket(Bucket bucket, String key) {
+		bucket.keySet.remove(key);
+		if (bucket.keySet.size() == 0) {
+			removeBucketFromList(bucket);
+			countToBucket.remove(bucket.count);
+		}
+	}
+
+	private void removeBucketFromList(Bucket bucket) {
+		bucket.prev.next = bucket.next;
+		bucket.next.prev = bucket.prev;
+		bucket.next = null;
+		bucket.prev = null;
+	}
+
+	private void addBucketAfter(Bucket newBucket, Bucket prevBucket) {
+		newBucket.prev = prevBucket;
+		newBucket.next = prevBucket.next;
+		prevBucket.next.prev = newBucket;
+		prevBucket.next = newBucket;
+	}
 }
