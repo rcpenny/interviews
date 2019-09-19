9fd28373fd500d31b954400a19faf4c18a8bc841
Daniel Rong <crong@crong-macbookpro2.roam.corp.google.com>
2019-09-18 22:38:17 -0700

    spiral matrix

--- company/罗宾汉/SpiralMatrix.java
+++ company/罗宾汉/SpiralMatrix.java
@@ -14,51 +14,52 @@ import java.util.List;
 
 class SpiralMatrix {
   public List<Integer> spiralOrder(int[][] matrix) {
-    List<Integer> result = new ArrayList<>();
+    List<Integer> result = new ArrayList<Integer>();
 
-    if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
-      return result;
-    
-    // 向右，下，左，上
-    int[] direction = {0,1,2,3};
+    if (matrix == null || matrix.length == 0) return result;
 
     int m = matrix.length;
     int n = matrix[0].length;
 
-    int d = 0;
-    
-    int i = 0, j = 0, count = 0;
-    while (count < m * n) {
-        // keep going within current direction until being out of border
-        while (i >= 0 && i < m && j >= 0 && j < n && matrix[i][j] != Integer.MIN_VALUE) {
-            res.add(matrix[i][j]);
-            matrix[i][j] = Integer.MIN_VALUE; // mark as processed
-            count++;
-            
-            if (direction == 0) j++; // go right
-            else if (direction == 1) i++; // go down
-            else if (direction == 2) j--; // go left
-            else if (direction == 3) i--; // go up
-        }
-        // out of border: change direction
-        direction = (direction + 1) % 4;
-        
-        // need to adjust to next valid number
-        if (direction == 1) { // down
-            j--;
-            i++;
-        } else if (direction == 2) { // left
-            i--;
-            j--;
-        } else if (direction == 3) { // up
-            j++;
-            i--; 
-        } else if (direction == 0) { // right
-            i++;
-            j++;
-        }
+    int x = 0; 
+    int y = 0;
+
+    while(m > 0 && n > 0){
+      //if one row/column left, no circle can be formed
+      if(m == 1){
+        for (int i = 0; i < n; i++)
+          result.add(matrix[x][y++]);
+        break;
+      } else if (n == 1) {
+        for(int i = 0; i < m; i++)
+          result.add(matrix[x++][y]);
+        break;
+      }
+
+      //below, process a circle
+
+      //top - move right
+      for (int i = 0; i < n - 1; i++)
+        result.add(matrix[x][y++]);
+
+      //right - move down
+      for (int i = 0; i < m - 1; i++)
+        result.add(matrix[x++][y]);
+
+      //bottom - move left
+      for (int i = 0; i < n - 1; i++)
+        result.add(matrix[x][y--]);
+
+      //left - move up
+      for(int i = 0; i < m - 1; i++)
+        result.add(matrix[x--][y]);
+
+      x++;
+      y++;
+      m -= 2;
+      n -= 2;
     }
-    
+
     return result;
   }
 }
