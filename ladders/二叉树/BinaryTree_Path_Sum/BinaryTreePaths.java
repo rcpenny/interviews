23466e69f86ae46ed11806d9bad7aed8ce52082b
Chen Rong <crong@google.com>
2019-09-21 19:00:19 -0700

    tree 1st

--- ladders/二叉树/BinaryTree_Path_Sum/BinaryTreePaths.java
+++ ladders/二叉树/BinaryTree_Path_Sum/BinaryTreePaths.java
@@ -1,29 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * https://www.lintcode.com/problem/binary-tree-paths/
- * 
+/** 
  * Given a binary tree, return all root-to-leaf paths.
  * Input：{1,2,3,#,5}
  * Output：["1->2->5","1->3"]
  */
-
-class TreeNode {
-	public int val;
-	public TreeNode left, right;
-	public TreeNode(int val) {
-		this.val = val;
-		this.left = this.right = null;
-	}
-}
 
 public class BinaryTreePaths {
 	private List<String> results = new ArrayList<>();	
 
   public List<String> binaryTreePaths(TreeNode root) {
 		if (root == null) return results;
+
 		helper(root, String.valueOf(root.val));
+
 		return results;
 	}
 
