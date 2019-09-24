45b2e1c05bf71ebdd68f93e1bcf8500db76f99a4
Chen Rong <crong@google.com>
2019-09-24 13:53:46 -0700

    cool

--- company/脸书/MEDIUM/RangeSumQuery2D.java
+++
@@ -1,9 +0,0 @@
-//Given a 2D matrix, find the sum of the elements inside the rectangle 
-// defined by its upper left corner (row1, col1) 
-// and lower right corner (row2, col2).
-
-@Todo
-final class RangeSumQuery2D {
-  private RangeSumQuery2D() {
-  }
-}
