5bf6c2b882eaa260b01b123061de85321b9331db
Chen Rong <crong@google.com>
2019-09-25 10:15:16 -0700

    动态规划，道阻且长

--- ladders/动态规划/DP0-记忆化/WordBreak2.java
+++ ladders/动态规划/DP0-记忆化/WordBreak2.java
@@ -5,11 +5,6 @@
 // 输出：["lint code", "lint co de"]
 // 解释：插入一个空格是"lint code"，插入两个空格是 "lint co de"
 
-// 1. 确定状态 memo[i][j] 前i-j个字符是否可以拼成words
-// 2. 转移方程 memo[i][j] = TRUE if exists memo[j] == TRUE and s(j...i) IN DICT (j < i)
-// 3. 初态边界 memo[0] = TRUE, 这样就能看所有从头到i位置的substring是否是word
-// 4. 顺序计算 memo[0] -> memo[i + 1]
-
 public class WordBreak2 {
   public List<String> wordBreak(String s, Set<String> wordDict) {
 		
