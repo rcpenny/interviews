b7cd68067412509fb1fd976a391406acea0e915a
Chen Rong <crong@google.com>
2019-09-25 10:03:35 -0700

    work break

--- ladders/动态规划/DP0-记忆化/WordBreak.java
+++ ladders/动态规划/DP0-记忆化/WordBreak.java
@@ -1,3 +1,5 @@
+import java.util.Set;
+
 // 给定字符串 s 和单词字典 dict，确定 s 是否可以分成一个或多个以空格分隔的子串，并且这些子串都在字典中存在。
 // 样例 1: 输入:  "lintcode", ["lint", "code"] 输出:  true
 
@@ -10,22 +12,21 @@
 
 public class WordBreak {
   public boolean wordBreak(String s, Set<String> dict) {
-    if (s == null || s.length() == 0) return false;
+		if (s == null) return false;
 
-    int n = s.length();
-    boolean[] memo = new boolean[n + 1];
-    memo[0] = true;
-    
-    for (int i = 1; i < n + 1; i++) {
-      for (int j = 0; j < i; j++) {
-        String tmp = s.substring(j, i);
-        if (memo[j] && dict.contains(tmp)) {
-          memo[i] = true;
-          break;
-        }
-      }
-    }
+		int n = s.length();
+		boolean[] f = new boolean[n + 1];
+		f[0] = true;
 
-    return memo[n];
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < i; j++) {
+				String sub = s.substring(j, i);
+				if (!dict.contains(sub) || f[j] == false) continue;
+				f[i] = true;
+				break;
+			}
+		}
+
+		return f[n];
   }
 }
