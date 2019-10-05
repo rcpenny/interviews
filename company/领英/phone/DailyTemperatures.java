a428c4606ae68adf9183054d0757e966ac2e1afe
Chen Rong <crong@google.com>
2019-10-05 15:51:38 -0700

    phone and onsite

---
+++ company/领英/phone/DailyTemperatures.java
@@ -0,0 +1,10 @@
+// 给定一个每日temperatures的列表，产生一个列表，对于输入的每天，告诉我们你要等多少天才能够等到一个更高的温度。
+// 如果没有可能的未来日期，输出0作为替代
+// 比如，给定列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是[1, 1, 4, 2, 1, 1, 0, 0]。
+
+// lc739
+public class DailyTemperatures {
+	public int[] dailyTemperatures(int[] temperatures) {
+		
+	}
+}
---
+++ company/领英/phone/OneEditDistance.java
@@ -0,0 +1,10 @@
+
+// 给你两个字符串 S 和 T, 判断他们是否只差一步编辑。
+// 输入: s = "aDb", t = "adb" 
+// 输出: true
+
+public class OneEditDistance {
+  public boolean isOneEditDistance(String s, String t) {
+		
+	}
+}
