0ee15a46bc73e87fbb240dbdd0c965dff8ea8209
Chen Rong <crong@google.com>
2019-11-09 10:32:41 -0800

    text justification

--- company/领英/算法题/ArrayStringMath/TextJustification.java
+++ company/领英/算法题/ArrayStringMath/TextJustification.java
@@ -18,75 +18,83 @@ import java.util.ArrayList;
 //               因为最后一行必须左对齐而不是完全对齐。
 //               请注意，第二行也是左对齐的，因为它只包含一个单词
 
+//leet68
 public class TextJustification {
   public List<String> fullJustify(String[] words, int maxWidth) {
-    List<String> res = new ArrayList<>();
-    if (words == null || words.length == 0) return res;
-    
-    List<String> list = new ArrayList<>();
-    
-    int len = 0;
+    List<String> text = new ArrayList<>();
 
-    for (String word: words) {
-      // 加words: 单词总长 + 空格数（最少保证一个空格) + 当前单词 <= maxWidth
-      if (list.isEmpty() || len + list.size() + word.length() <= maxWidth) {
-          list.add(word);
-          len += word.length();
-          continue;
+    if (words == null || words.length == 0) {
+      return text;
+    }
+
+    int curLen = 0;
+    List<String> curWords = new ArrayList<>();
+
+    for (String word : words) {
+
+      // 1. don't assemble words, add words greedyly to a line
+      if (curWords.isEmpty() || curLen + curWords.size() + word.length() <= maxWidth) {
+        curWords.add(word);
+        curLen += word.length();
+        continue;
       }
 
-      // 拼words
+      // 2. assemable words
+      int spaceCount = maxWidth - curLen;
+      int interval = curWords.size() - 1;
       StringBuilder sb = new StringBuilder();
-      int spaceCnt = maxWidth - len;
-      int interval = list.size() - 1;
 
-      // 一个单词
+        // only 1 word
       if (interval == 0) {
-        sb.append(list.get(0));
-        for (int j = 0; j < spaceCnt; ++j) {
-            sb.append(" ");
-        }   
-      } else {
-        int extraSpace = spaceCnt % interval;
-        int spacesEachInterval = spaceCnt / interval;
-        
+        sb.append(curWords.get(0));
+        for (int i = 0; i < spaceCount; i++) sb.append(" ");
+      }
+
+        // more than 1 word
+      else {
+        int extrasSpace = spaceCount % interval;
+        int spaceEachInterval = spaceCount / interval;
+
         String spaces = "";
-        for (int k = 0; k < spacesEachInterval; ++k) {
-            spaces += " ";
+        for (int k = 0; k < spaceEachInterval; k++) {
+          spaces += " ";
         }
-        
-        sb.append(list.get(0));
-        for (int j = 1; j < list.size(); ++j) {
+
+        sb.append(curWords.get(0));
+
+        for (int j = 1; j < curWords.size(); j++) {
           sb.append(spaces);
-          if (extraSpace != 0) {
-              sb.append(" ");
-              extraSpace--;
+          if (extrasSpace != 0) {
+            sb.append(" ");
+            extrasSpace--;
           }
-          sb.append(list.get(j));
+          sb.append(curWords.get(j));
         }
       }
 
-      res.add(sb.toString());
-      len = word.length();
-      list = new ArrayList<>();
-      list.add(word);
+
+      // 3. after assemble, add line to text,
+      text.add(sb.toString());
+      curLen = word.length();
+      curWords.clear();
+      curWords.add(word);
     }
-    
-    //last line
+
+
+    // take care of last line
     StringBuilder sb = new StringBuilder();
-    int spaceCnt = maxWidth - len;
-    sb.append(list.get(0));
-    for (int j = 1; j < list.size(); ++j) {
-        sb.append(" ");
-        sb.append(list.get(j));
+    sb.append(curWords.get(0));
+
+    for (int j = 1; j < curWords.size(); j++) {
+      sb.append(" ");
+      sb.append(curWords.get(j));
     }
-    
-    int spaceLeft = maxWidth - sb.length();
-    for (int k = 0; k < spaceLeft; ++k) {
-        sb.append(" ");
+
+    while (sb.length() < maxWidth) {
+      sb.append(" ");
     }
-    res.add(sb.toString());
-    
-    return res;
+    text.add(sb.toString());
+
+    return text;
   }
 }
