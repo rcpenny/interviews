97a452794cd3b0b56e476e74b84d805e69cc04de
Chen Rong <crong@google.com>
2019-11-10 11:13:12 -0800

    delay task scheduler

--- company/领英/系统设计/DelayQueue.java
+++ company/领英/系统设计/DelayQueue.java
@@ -1,116 +1,231 @@
-import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.*;
 
-public class DelayQueue<E extends Delayed> extends AbstractQueue<E>
-    implements BlockingQueue<E> {
+public class DelayQueue<E extends Delayed> {
+	private final transient ReentrantLock lock = new ReentrantLock();
 
-    private final transient ReentrantLock lock = new ReentrantLock();
-    private final PriorityQueue<E> q = new PriorityQueue<E>();
+	private final PriorityQueue<E> queue = new PriorityQueue<E>();
 
-    /**
-     * Thread designated to wait for the element at the head of
-     * the queue.  This variant of the Leader-Follower pattern
-     * (http://www.cs.wustl.edu/~schmidt/POSA/POSA2/) serves to
-     * minimize unnecessary timed waiting.  When a thread becomes
-     * the leader, it waits only for the next delay to elapse, but
-     * other threads await indefinitely.  The leader thread must
-     * signal some other thread before returning from take() or
-     * poll(...), unless some other thread becomes leader in the
-     * interim.  Whenever the head of the queue is replaced with
-     * an element with an earlier expiration time, the leader
-     * field is invalidated by being reset to null, and some
-     * waiting thread, but not necessarily the current leader, is
-     * signalled.  So waiting threads must be prepared to acquire
-     * and lose leadership while waiting.
-     */
-    private Thread leader = null;
+	/**
+	 * Thread designated to wait for the element at the head of
+	 * the queue.  This variant of the Leader-Follower pattern
+	 * (http://www.cs.wustl.edu/~schmidt/POSA/POSA2/) serves to
+	 * minimize unnecessary timed waiting.  When a thread becomes
+	 * the leader, it waits only for the next delay to elapse, but
+	 * other threads await indefinitely.  The leader thread must
+	 * signal some other thread before returning from take() or
+	 * poll(...), unless some other thread becomes leader in the
+	 * interim.  Whenever the head of the queue is replaced with
+	 * an element with an earlier expiration time, the leader
+	 * field is invalidated by being reset to null, and some
+	 * waiting thread, but not necessarily the current leader, is
+	 * signalled.  So waiting threads must be prepared to acquire
+	 * and lose leadership while waiting.
+	 */
+	// 线程leader的作用
+	// leader这个成员有啥作用？DelayQueue的设计其实是一个Leader/Follower模式，
+	// leader就是指向Leader线程的。该模式可以减少不必要的等待时间，
+	// 当一个线程是Leader时，它只需要一个时间差； 其他Follower线程则无限等待。
+	// 比如头节点任务还有5秒就要开始了，那么Leader线程会sleep 5秒，不需要傻傻地等待固定时间间隔。
+	private Thread leader = null;
 
-    /**
-     * Condition signalled when a newer element becomes available
-     * at the head of the queue or a new thread may need to
-     * become leader.
-     */
-    private final Condition available = lock.newCondition();
+	/**
+	 * Condition signalled when a newer element becomes available
+	 * at the head of the queue or a new thread may need to
+	 * become leader.
+	 */
+	private final Condition available = lock.newCondition();
 
-    /**
-     * Creates a new {@code DelayQueue} that is initially empty.
-     */
-    public DelayQueue() {}
+	/** Creates a new {@code DelayQueue} that is initially empty. */
+	public DelayQueue() {}
 
-    /**
-     * Creates a {@code DelayQueue} initially containing the elements of the
-     * given collection of {@link Delayed} instances.
-     *
-     * @param c the collection of elements to initially contain
-     * @throws NullPointerException if the specified collection or any
-     *         of its elements are null
-     */
-    public DelayQueue(Collection<? extends E> c) {
-        this.addAll(c);
-    }
+	/**
+	 * Inserts the specified element into this delay queue.
+	 *
+	 * @param e the element to add
+	 * @return {@code true}
+	 * @throws NullPointerException if the specified element is null
+	 */
+	public boolean offer(E e) {
+		final ReentrantLock offerLock = this.lock;
+		// acquire the lock
+		offerLock.lock();
+		try {
+			queue.offer(e);
+			// 如果第一个元素等于刚刚插入进去的元素，说明刚才队列是空的。
+			// 现在队列里有了一个任务，那么就应该唤醒所有在等待的消费者线程，
+			// 避免了PriorityBlockingQueue + 时间差的缺点
+			// 将leader重置为null，这些消费者之间互相竞争，自然有一个会被选为leader
+			if (queue.peek() == e) {
+				leader = null;
+				// Wakes up one waiting thread.
+				available.signal();
+			}
+			return true;
+		} finally {
+			offerLock.unlock();
+		}
+	}
 
-    /**
-     * Inserts the specified element into this delay queue.
-     *
-     * @param e the element to add
-     * @return {@code true}
-     * @throws NullPointerException if the specified element is null
-     */
-    public boolean offer(E e) {
-        final ReentrantLock lock = this.lock;
-        lock.lock();
-        try {
-            q.offer(e);
-            if (q.peek() == e) {
-                leader = null;
-                available.signal();
-            }
-            return true;
-        } finally {
-            lock.unlock();
-        }
-    }
+	/**
+	 * Retrieves and removes the head of this queue, waiting if necessary
+	 * until an element with an expired delay is available on this queue.
+	 *
+	 * @return the head of this queue
+	 * @throws InterruptedException {@inheritDoc}
+	 */
+	public E take() throws InterruptedException {
+		final ReentrantLock takeLock = this.lock;
+		// Acquires the lock unless the current thread is interrupted.
+		lock.lockInterruptibly();
 
-    /**
-     * Retrieves and removes the head of this queue, waiting if necessary
-     * until an element with an expired delay is available on this queue.
-     *
-     * @return the head of this queue
-     * @throws InterruptedException {@inheritDoc}
-     */
-    public E take() throws InterruptedException {
-        final ReentrantLock lock = this.lock;
-        lock.lockInterruptibly();
-        try {
-            for (;;) {
-                E first = q.peek();
-                if (first == null)
-                    available.await();
-                else {
-                    long delay = first.getDelay(NANOSECONDS);
-                    if (delay <= 0)
-                        return q.poll();
-                    first = null; // don't retain ref while waiting
-                    if (leader != null)
-                        available.await();
-                    else {
-                        Thread thisThread = Thread.currentThread();
-                        leader = thisThread;
-                        try {
-                            available.awaitNanos(delay);
-                        } finally {
-                            if (leader == thisThread)
-                                leader = null;
-                        }
-                    }
-                }
-            }
-        } finally {
-            if (leader == null && q.peek() != null)
-                available.signal();
-            lock.unlock();
-        }
-    }
+		try {
+			for (;;) {
+				E first = queue.peek();
+				if (first == null) {
+					// Causes the current thread to wait until it is signalled or interrupted.
+					available.await();
+				} else {
+					long delay = first.getDelay(MILLISECONDS);
+					if (delay <= 0) return queue.poll();
+
+					// 如果删除这行代码，会发生什么呢？假设现在有3个消费者线程，
+					// 线程A进来获取first,然后进入 else 的 else ,设置了leader为当前线程A，并让A等待一段时间
+					// 线程B进来获取first, 进入else的阻塞操作,然后无限期等待，这时线程B是持有first引用的
+					// 线程A等待指定时间后被唤醒，获取对象成功，出队，这个对象理应被GC回收，但是它还被线程B持有着，GC链可达，所以不能回收这个first
+					// 只要线程B无限期的睡眠，那么这个本该被回收的对象就不能被GC销毁掉，那么就会造成内存泄露
+					first = null;
+					
+					// 想象一下有个多个消费者线程用take方法去取任务,内部先加锁,然后每个线程都去peek头节点。
+					// 如果leader不为空说明已经有线程在取了，让当前消费者无限等待。
+					if (leader != null) {
+						available.await();
+					} 
+					
+					// 如果为空说明没有其他消费者去取任务,设置leader为当前消费者，并让改消费者等待指定的时间
+					// 下次循环会走如下分支，取到任务结束，if (delay <= 0) return q.poll();
+					else {
+						Thread thisThread = Thread.currentThread();
+						leader = thisThread;
+
+						try {
+							available.await(delay, MILLISECONDS);
+						} finally {
+							if (leader == thisThread) leader = null;
+						}
+					}
+				}
+			}
+		} finally {
+			if (leader == null && queue.peek() != null) available.signal();
+			lock.unlock();
+		}
+	}
+}
+
+
+
+
+/** Actual Task */
+class Task {Task() {}}
+/** Delayed Task, implements Delayed. */
+class DelayTask implements Delayed {
+	private Task task;
+	private long duration;
+
+	public DelayTask(Task task, long delay) {
+		this.task = task;
+		this.duration = System.currentTimeMillis() + duration;
+	}
+
+	@Override
+	@Override
+	public long getDelay(TimeUnit unit) {
+		long diff = duration - System.currentTimeMillis();
+		return unit.convert(diff, TimeUnit.MILLISECONDS);
+	}
+
+	@Override
+	public int compareTo(DelayTask o) {
+		return this.duration - o.duration;
+	}
+}
+// public interface Delayed extends Comparable<Delayed> {
+// 	/**
+// 	 * Returns the remaining delay associated with this object, in the
+// 	 * given time unit.
+// 	 *
+// 	 * @param unit the time unit
+// 	 * @return the remaining delay; zero or negative values indicate
+// 	 * that the delay has already elapsed
+// 	 */
+// 	long getDelay(TimeUnit unit);
+// }
+
+
+
+
+/** Producer of DelayTask */
+// 生产者很简单，就是一个死循环，不断地产生一些是时间随机的任务。
+class DelayTaskProducer implements Runnable {
+	private final Random random = new Random();
+	private DelayQueue<DelayTask> queue;
+
+	public DelayTaskProducer(DelayQueue<DelayTask> queue) {
+		this.queue = queue;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				int delay = random.nextInt(10000);
+				DelayTask task = new DelayTask(new Task(), delay);
+				queue.offer(task);
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+
+
+
+/** Consumer of DelayTask */
+// 当 DelayQueue 里没有任务时，TaskConsumer会无限等待，直到被唤醒，因此它不会消耗CPU
+class DelayTaskConsumer implements Runnable {
+	private DelayQueue<DelayTask> queue;
+
+	public DelayTaskConsumer(DelayQueue<DelayTask> queue) {
+		this.queue = queue;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				DelayTask task = queue.take();
+				System.out.println(task);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+// 定时任务调度器
+class TaskScheduler {
+	public static void main(String[] args) {
+		DelayQueue<DelayTask> queue = new DelayQueue<>();
+
+		new Thread(new DelayTaskProducer(queue), "Producer thread").start();
+		new Thread(new DelayTaskConsumer(queue), "Consumer thread").start();
+	} 
 }
--- company/领英/系统设计/Delayed-Task-Scheduler.md
+++ company/领英/系统设计/Delayed-Task-Scheduler.md
@@ -22,7 +22,28 @@ https://soulmachine.gitbooks.io/system-design/content/cn/task-scheduler.html
 
 ## Actual code
 1. TaskScheduler class
-2. DelayedQueue class
-3. task class
-4. producer class
-5. conusmer class
+2. DelayQueue class
+3. Delaytask class
+4. DelayTaskProducer class
+5. DelayTaskconusmer class
+
+## 方案
+方案1: PriorityBlockingQueue + Polling
+我们很快可以想到第一个办法：
+用一个java.util.concurrent.PriorityBlockingQueue来作为优先队列。因为我们需要一个优先队列，又需要线程安全，用PriorityBlockingQueue再合适不过了。你也可以手工实现一个自己的PriorityBlockingQueue，用java.util.PriorityQueue + ReentrantLock，用一把锁把这个队列保护起来，就是线程安全的啦
+对于生产者，可以用一个while(true)，造一些随机任务塞进去
+对于消费者，起一个线程，在 while(true)里每隔几秒检查一下队列，如果有任务，则取出来执行。
+这个方案的确可行，总结起来就是轮询(polling)。轮询通常有个很大的缺点，就是时间间隔不好设置，间隔太长，任务无法及时处理，间隔太短，会很耗CPU。
+方案2: PriorityBlockingQueue + 时间差
+可以把方案1改进一下，while(true)里的逻辑变成：
+偷看一下堆顶的元素，但并不取出来，如果该任务过期了，则取出来
+如果没过期，则计算一下时间差，然后 sleep()该时间差
+不再是 sleep() 一个固定间隔了，消除了轮询的缺点。
+稍等！这个方案其实有个致命的缺陷，导致它比 PiorityBlockingQueue + Polling 更加不可用，这个缺点是什么呢？。。。假设当前堆顶的任务在100秒后执行，消费者线程peek()偷看到了后，开始sleep 100秒，这时候一个新的任务插了进来，该任务在10秒后应该执行，但是由于消费者线程要睡眠100秒，这个新任务无法及时处理。
+
+方案4: 时间轮(HashedWheelTimer)
+时间轮(HashedWheelTimer)其实很简单，就是一个循环队列，如下图所示，
+上图是一个长度为8的循环队列，假设该时间轮精度为秒，即每秒走一格，像手表那样，走完一圈就是8秒。每个格子指向一个任务集合，时间轮无限循环，每转到一个格子，就扫描该格子下面的所有任务，把时间到期的任务取出来执行。
+举个例子，假设指针当前正指向格子0，来了一个任务需要4秒后执行，那么这个任务就会放在格子4下面，如果来了一个任务需要20秒后执行怎么？由于这个循环队列转一圈只需要8秒，这个任务需要多转2圈，所以这个任务的位置虽然依旧在格子4(20%8+0=4)下面，不过需要多转2圈后才执行。因此每个任务需要有一个字段记录需圈数，每转一圈就减1，减到0则立刻取出来执行。
+怎么实现时间轮呢？Netty中已经有了一个时间轮的实现, HashedWheelTimer.java，可以参考它的源代码。
+时间轮的优点是性能高，插入和删除的时间复杂度都是O(1)。Linux 内核中的定时器采用的就是这个方案。
